b2d636dd19e34cd4a8b9221a80fe558e05b67501
Gustavo Graf de Sousa <graf.sousa@gmail.com>
2019-01-21 22:23:11 -0200

    Melhorando o visual do TouchableHighlight

--- app5/src/components/CenaPrincipal.js
+++ app5/src/components/CenaPrincipal.js
@@ -31,6 +31,8 @@ export default class CenaPrincipal extends Component {
         <View style={styles.menu}>
           <View style={styles.menuGrupo}>
             <TouchableHighlight
+              underlayColor={'#B9C941'}
+              activeOpacity={0.3}
               onPress={() => {
                 this.props.navigator.push({ id: 'cliente' });
               }}
@@ -38,25 +40,31 @@ export default class CenaPrincipal extends Component {
               <Image style={styles.imgMenu} source={menuCliente} />
             </TouchableHighlight>
             <TouchableHighlight
+              underlayColor={'#61BD8C'}
+              activeOpacity={0.3}
               onPress={() => {
                 this.props.navigator.push({ id: 'contatos' });
               }}
             >
-              <Image style={styles.imgMenu} source={menuContato} /> 
+              <Image style={styles.imgMenu} source={menuContato} />
             </TouchableHighlight>
 
           </View>
           <View style={styles.menuGrupo}>
 
-          <TouchableHighlight
+            <TouchableHighlight
+              underlayColor={'#EC7148'}
+              activeOpacity={0.3}
               onPress={() => {
                 this.props.navigator.push({ id: 'empresa' });
               }}
             >
               <Image style={styles.imgMenu} source={menuEmpresa} />
             </TouchableHighlight>
-            
+
             <TouchableHighlight
+              underlayColor={'#19D1C8'}
+              activeOpacity={0.3}
               onPress={() => {
                 this.props.navigator.push({ id: 'servico' });
               }}
